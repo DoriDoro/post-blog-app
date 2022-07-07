3051b977e12a355305e96e8ceaf5196ff15cac10
DoriDoro <dorothea.hassan@gmail.com>
2022-07-07 23:56:52 +0200

    modify app

--- post_blog/post_blog_app/apps.py
+++ post_blog/post_blog_app/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class PostBlockAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'post_block_app'
+    name = 'post_blog_app'
