df705fa4b5abf93fbe942ba015ea9791d59106e5
DoriDoro <dorothea.hassan@gmail.com>
2022-07-08 00:03:20 +0200

    modify asgi

--- post_blog/post_blog_project/asgi.py
+++ post_blog/post_blog_project/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'post_block_project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'post_blog_project.settings')
 
 application = get_asgi_application()
