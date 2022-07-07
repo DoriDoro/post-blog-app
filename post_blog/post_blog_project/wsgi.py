4fb56af3a8af4d2c685349ecd70531702a69582a
DoriDoro <dorothea.hassan@gmail.com>
2022-07-08 00:04:12 +0200

    modify wsgi

--- post_blog/post_blog_project/wsgi.py
+++ post_blog/post_blog_project/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'post_block_project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'post_blog_project.settings')
 
 application = get_wsgi_application()
